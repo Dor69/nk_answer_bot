621302ade4d2cdf32161bc377c4244071d05cc70
dor69 <cat.of.mat@gmail.com>
2023-02-21 07:10:55 -0500

    release v1

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ storage = MemoryStorage()
 bot = Bot(token= BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=storage)
 state = None
-
-#data =[]
-
 #Method /Start
 
 @dp.message_handler(commands=["start"])
@@ -28,9 +25,9 @@ async def message_handler1(message):
  #Method send photo to admin
 @dp.message_handler(content_types=["photo"])
 async def replying(pic):
-    await bot.send_photo(admin_id, pic.photo[-1].file_id)
     await bot.send_message(text="Картинка от @{} подпись = {} ".format(pic.from_user["username"], pic.caption), chat_id = admin_id)
-
+    await bot.send_photo(admin_id, pic.photo[-1].file_id)
+    
 
 if __name__ == '__main__':
     executor.start_polling(dp)
